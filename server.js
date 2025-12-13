179d442f0934bc9f9bcac9f24aac1baf92c82524
Umair93r <rangrezumair286@gmail.com>
2025-12-13 08:17:02 +0000

    13 update

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ app.post("/api/admin/timings", basicAuth, (req, res) => {
 
 // Route for /admin (login popup)
 app.get("/admin", basicAuth, (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "admin.html"));
+  res.sendFile(path.join(__dirname, "admin", "admin.html"));
 });
 
 app.listen(PORT, () => {
